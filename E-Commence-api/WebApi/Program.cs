e4ec279a92b3855a13acc71090752b754da81756
Mohamed Abdraboh <Mohamed.Abdraboh@advansys-esc.com>
2023-06-20 13:21:27 +0300

    AllowAnyOrigin

--- E-Commence-api/WebApi/Program.cs
+++ E-Commence-api/WebApi/Program.cs
@@ -7,6 +7,8 @@ using Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
 // Add services to the container.
 builder.Services
     .AddApplication()
